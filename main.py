5e2b6e8b9654b6e980a9d8931fa52ab4b881d746
dev-manager <factorio@naver.com>
2021-09-04 21:56:37 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+from map_maker import maker
+import time
+import sys
+
+
+def Block(sudoku, m, n):
+    a = int(len(sudoku) ** .5)
+    M = a * (m // a + 1)
+    N = a * (n // a + 1)
+    MM = max(0, M - a)
+    NN = max(0, N - a)
+    return list(map(lambda x: x[NN:N], sudoku[MM:M]))
+
+
+def isValid(sudoku, m, n, v):
+    a = sudoku[:]
+    for i in a[m]:
+        if i == v:
+            return False
+    for i in range(len(a)):
+        if a[i][n] == v:
+            return False
+    T = ''.join(map(lambda x: ''.join(map(str, x)), Block(a, m, n))).replace('0', '')
+    for i in T:
+        if T.count(i) - 1:
+            return False
+    
+    return True
+
+
+def solver(su, Z=0, start: float = 0.0):
+    global solved
+    global file_name
+    solved = ''
+    sudoku = su[:]
+    
+    m = Z // len(sudoku)
+    n = Z % len(sudoku)
+    
+    if Z == len(sudoku) ** 2:
+        for i in sudoku:
+            print('  '.join(map(str, i)))
+            solved += '  '.join(map(str, i)) + '\n'
+        file_name = board.replace('  ', '')[:9]
+        with open("E:/PRAC/test_cases/solved_board/solved_board_" + file_name + ".board", 'w') as f:
+            f.write(solved)
+        times = round(time.time() - start, 3)
+        print(f"solving took {times}sec")
+        with open("E:/PRAC/test_cases/times/solving_time_" + file_name + ".board", 'w') as f:
+            f.write(f"solving took {times}sec")
+        sys.exit(Z)
+    
+    if sudoku[m][n]:
+        solver(sudoku, Z + 1, start=start)
+    else:
+        for i in range(1, len(sudoku) + 1):
+            if isValid(sudoku, m, n, i):
+                sudoku[m][n] = i
+                solver(sudoku, Z + 1, start=start)
+                sudoku[m][n] = 0
+
+
+def solving():
+    global board
+    global file_name
+    valid = False
+    board = ''
+    while not valid:
+        board = maker()
+        try:
+            board.replace("  ", "").index("000000000")
+        except ValueError:
+            valid = True
+        else:
+            pass
+       
+    file_name = board.replace('  ', '')[:9]
+    with open("E:/PRAC/test_cases/unsolved_board/unsolved_board_" + file_name + ".board", 'w') as f:
+        f.write(board)
+    print(f"file_name:{file_name}")
+    print("board:")
+    print(board)
+    print(f"zero count: {board.count('0')}")
+    board = board.replace("  ", '')
+    print("solved:")
+    sudoku = list(map(lambda x: list(map(int, list(x))), board.split("\n")))
+    solver(sudoku, start=time.time())
+
+
+solving()
---
+++ map_maker.py
@@ -0,0 +1,32 @@
+from random import sample
+
+
+def maker():
+    base = 3
+    side = base * base
+    
+    def pattern(r, c):
+        return (base * (r % base) + r // base + c) % side
+    
+    def shuffle(s):
+        return sample(s, len(s))
+    
+    r_base = range(base)
+    rows = [g * base + r for g in shuffle(r_base) for r in shuffle(r_base)]
+    cols = [g * base + c for g in shuffle(r_base) for c in shuffle(r_base)]
+    nums = shuffle(range(1, base * base + 1))
+    
+    # produce board using randomized baseline pattern
+    board = [[nums[pattern(r, c)] for c in cols] for r in rows]
+    
+    squares = side * side
+    empties = squares * 3 // 4
+    for p in sample(range(squares), empties):
+        board[p // side][p % side] = 0
+    
+    num_size = len(str(side))
+    sudoku_map = ''
+    for line in board:
+        sudoku_map += "" + "  ".join(f"{n or '0':{num_size}}" for n in line) + ""
+        sudoku_map += "\n"
+    return sudoku_map[:-1]
